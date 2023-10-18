051fac38805eab2da27b8be57ddfc777d291e3ee
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-10-17 21:22:22 -0500

    Port over content from kb (#10)
    Signed-off-by: GitHub <noreply@github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,15 +22,8 @@ export default defineConfig({
 					link: '/'
 				},
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'World Ship',
+					autogenerate: { directory: 'world-ship' },
 				},
 			],
 		}),
